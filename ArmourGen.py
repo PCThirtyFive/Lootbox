34b10095d8c49b1489b2b8c796ed629ccae229c1
PC35 <housechesh@gmail.com>
2025-04-19 18:44:21 -0700

    initial start

--- ArmourGen.py
+++ ArmourGen.py
@@ -1,27 +1,219 @@
 import random
-import generateAname()
+import namegen
+import json
+import sqlite3
+from namegen import generateAname
+
 slots = ["Head", "Shoulder", "Arm", "Hand", "Chest", "Pants", "Boots"]
 armourtypes = {"Heavy" :4,
                "Medium":3,
                "Light":2,
                "Cloth":1}
 
+setbonus = ["Orion the Hunter", "The Beastmaster", "The Berserker", "The Gladiator", "The Duelist", "The Assassin",
+            "The Rogue", "The Trickster", "The Shadow", "The Nightblade", "The Phantom", "The Specter", "The Wraith",
+            "The Revenant", "The Lich", "The Necromancer", "The Warlock", "The Sorcerer", "The Wizard", "The Mage",
+            "The Alchemist", "The Enchanter", "The Illusionist", "The Elementalist", "The Pyromancer", "The Cryomancer",
+            "The Geomancer", "The Aeromancer", "The Hydromancer"]
 
 
+def setbonuscalc():
+    """
+    Generates a set bonus for the armour item.
+    """
+    #check whether a setbonus should be applied
+    if random.randint(0, 10) == 5:
+        # Choose a random set bonus from the list
+        return random.choice(setbonus)
+    else:
+        return None
+
+def specprocgen():
+    """
+    Generates a special proc for the armour item.
+    """
+    # Define possible special procs
+    special_procs = ["Kinetic", "Heat","Energy", "Projectile", "Electric","Cold","Acid","Explosive","Exotic","Dark","Sonic","Water","Devine"]
+    #choose 1, 2 or 3 special procs based on a weighting system
+    spng = random.randint(0, 100)
+    if spng < 5: # 5% chance of getting no special
+        return None
+    elif spng < 45: # 40% chance of getting one special
+        return random.choice(special_procs)
+    elif spng < 90: # 45% chance of getting two specials
+        return random.sample(special_procs, 2)
+    else: # 10% chance of getting three specials
+        return random.sample(special_procs, 3)
+
+def modifiergen():
+    """
+    Generates a random modifier for the armour item.
+    """
+    # Define possible modifiers
+    modifiers = ["Critical Hit Chance", "Critical Hit Damage", "Two Handed Melee Speed", "One Handed Melee Speed", "Unarmed Melee Speed",
+                 "Pistol Attack Speed", "Carbine Attack Speed","Rifle Attack Speed", "Heavy Weapon Speed", "Melee Defence", "Ranged Toughness"]
+    ded = random.randint(0, 100)
+    if ded > 96:
+        return random.choice(modifiers)
+    else:
+        return None
+
+def modifiergen2():
+    """
+    Generates a random modifier for the armour item.
+    """
+    # Define possible modifiers
+    modifiers = ["Critical Hit Chance", "Critical Hit Accuracy", "Two Handed Melee Accuracy", "One Handed Melee Accuracy", "Unarmed Melee Accuracy",
+                 "Pistol Attack Accuracy", "Carbine Attack Accuracy","Rifle Attack Accuracy", "Heavy Weapon Accuracy", "Ranged Defence", "Melee Toughness"]
+    ded = random.randint(0, 100)
+    if ded > 96:
+        return random.choice(modifiers)
+    else:
+        return None
+
+def modifiervalgen(modifier):
+
+    if modifier == None:
+        return None
+    else:
+        return random.randint(1, 25)
+
+def vulngen(specproc):
+    """
+    Generates a random number of vulnerability for the armour item 0-3:
+    """
+    vulnist = ["Kinetic", "Heat","Energy", "Projectile", "Electric","Cold","Acid","Explosive","Exotic","Dark","Sonic","Water","Devine"]
+    specproc1 = specproc
+    # Remove the special proc from the list of possible vulnerabilities
+    if specproc1 is not None:
+        if isinstance(specproc1, list):
+            for proc in specproc1:
+                vulnist.remove(proc)
+        else:
+            vulnist.remove(specproc1)
+    vlng = random.randint(0, 100)
+    if vlng < 5: # 5% chance of getting no vulnerability
+        return None
+    elif vlng < 55: # 40% chance of getting one vulnerability
+        return random.choice(vulnist)
+    else: # 45% chance of getting two vulnerabilities
+        return random.sample(vulnist, 2)
+
+def basegen(specproc, vuln):
+    """
+    Generates a base resistance for the armour item.
+    """
+    # Define possible base resistances
+    base_resistances = ["Kinetic", "Heat","Energy", "Projectile", "Electric","Cold","Acid","Explosive","Exotic","Dark","Sonic","Water","Devine"]
+    # Remove the special proc and vulnerability from the list of possible resistances
+    if specproc is not None:
+        if isinstance(specproc, list):
+            for proc in specproc:
+                base_resistances.remove(proc)
+        else:
+            base_resistances.remove(specproc)
+    if vuln is not None:
+        if isinstance(vuln, list):
+            for proc in vuln:
+                base_resistances.remove(proc)
+        else:
+            base_resistances.remove(vuln)
+    # Choose a random base resistance from the list
+    return base_resistances
+
+def setbonuscalc():
+    """
+    Generates a set bonus for the armour item.
+    """
+    #check whether a setbonus should be applied
+    if random.randint(0, 10) == 5:
+    # Choose a random set bonus from the list
+        return random.choice(setbonus)
+    else:
+        return None
 
 class armour:
-    def __init__(self, name, slot, type, typeval, specproc, specres, baseproc, baseres, vuln, condtion, socket, modifiers,setbonus):
-        self.condtion = random.randint(10000, 95000)
+    """
+    Represents an armour item with various attributes.
+    """
+    def __init__(self, name, slot, type, typeval, specproc, specres, baseproc, baseres, vuln, condition, socket, modifiers1, modifiers2, modifiers1v, modifiers2v, setbonus, rating):
+        self.condition = random.randint(10000, 95000)
         self.type = random.choice(list(armourtypes.keys()))
         self.typeval = armourtypes[self.type]
         self.slot = random.choice(slots)
-        self.name = generateAname(type,slot)
-        self.specproc = specproc
-        self.specres = specres
-        self.baseproc = baseproc
+        self.specproc = specprocgen()
+        self.specres = random.randint(76, 95)
+        self.vuln = vulngen(self.specproc)
+        self.baseproc = basegen(self.specproc, self.vuln)
         self.baseres = random.randint(20, 75)
-        self.vuln = vuln
         self.socket = random.randint(0, 2)
-        self.modifiers = modifiers
-        self.setbonus = setbonus
+        self.modifiers1 = modifiergen()
+        self.modifiers2 = modifiergen2()
+        self.modifiers1v = modifiervalgen(self.modifiers1)
+        self.modifiers2v = modifiervalgen(self.modifiers2)
+        self.setbonus = setbonuscalc()
 
+        #for rating calculation
+        if self.modifiers1 is None:
+            modifiers1_count = 0
+        else:
+            modifiers1_count = 1
+        if self.modifiers2 is None:
+            modifiers2_count = 0
+        else:
+            modifiers2_count = 1
+        if self.setbonus is None:
+            setbonus_count = 0
+        else:
+            setbonus_count = 1
+        if self.modifiers1v is None:
+            modifiers1v_count = 0
+        else:
+            modifiers1v_count = self.modifiers1v
+        if self.modifiers2v is None:
+            modifiers2v_count = 0
+        else:
+            modifiers2v_count = self.modifiers2v
+
+        self.rating = int(
+            ((self.condition / 95000) +
+            (self.typeval / 4) +
+            (len(self.specproc) / 3) +  # Count the number of special procs
+            (self.specres / 95) +
+            (self.baseres / 75) +
+            (self.socket / 2) +
+            ((modifiers1_count /2 + 0)) +
+            ((modifiers2_count / 2) + 0) +
+            ((modifiers1v_count / 25) + 0) +
+            ((modifiers2v_count / 25) + 0) +
+            ((setbonus_count / 1) + 0)) / 11 * 100)
+
+        self.name = generateAname(self.type,self.slot,self.setbonus,self.rating,self.specproc)
+
+def generate_armour():
+    """
+    Generates a random armour item.
+    """
+    # Generate a random armour item
+    return armour(None, None, None, None, None, None, None,
+                  None, None, None, None, None, None, None,
+                  None, None, None)
+def print_armour(armour_item):
+    print(f"Name: {armour_item.name}")
+    print(f"Slot: {armour_item.slot}")
+    print(f"Type: {armour_item.type}")
+    print(f"Condition: {armour_item.condition}")
+    print(f"Special Proc: {armour_item.specproc}")
+    print(f"Special Resistance: {armour_item.specres}")
+    print(f"Vulnerability: {armour_item.vuln}")
+    print(f"Base Proc: {armour_item.baseproc}")
+    print(f"Base Resistance: {armour_item.baseres}")
+    print(f"Socket: {armour_item.socket}")
+    print(f"Modifiers 1: {armour_item.modifiers1} ({armour_item.modifiers1v})")
+    print(f"Modifiers 2: {armour_item.modifiers2} ({armour_item.modifiers2v})")
+    print(f"Set Bonus: {armour_item.setbonus}")
+    print(f"Rating: {armour_item.rating}")
+
+
+for _ in range(100):
+    print_armour(generate_armour()) #generate and print a random armour item
--- namegen.py
+++ namegen.py
@@ -1,42 +1,100 @@
 import random
 
-def generateAname(type, slot):
+def generateAname(type, slot, setbonus, rating, specproc):
     """
     Generates a name for an armour item based on its type and slot.
     """
     # Define possible prefixes and suffixes for the armour item name
-    prefixes = ["Ancient", "Mystic", "Cursed", "Enchanted", "Divine", "Fabled", "Legendary", "Epic", "Rare", "Common", "Uncommon", "Mythic", "Heroic", "Arcane", "Celestial", "Infernal", "Ethereal", "Spectral", "Phantom", "Wraith", "Void", "Titanic", "Dragonkin", "Demonic", "Celestial", "Elemental", "Arcane", "Runic", "Shadow", "Lightbringer", "Darkbane", "Stormforged",
+    sufffixes = ["Ancient", "Mystic", "Cursed", "Enchanted", "Divine", "Fabled", "Legendary", "Epic", "Rare", "Common", "Uncommon", "Mythic", "Heroic", "Arcane", "Celestial", "Infernal", "Ethereal", "Spectral", "Phantom", "Wraith", "Void", "Titanic", "Dragonkin", "Demonic", "Celestial", "Elemental", "Arcane", "Runic", "Shadow", "Lightbringer", "Darkbane", "Stormforged",
                     "Frostforged", "Flameforged", "Earthshatter", "Thunderstrike", "Bloodbound", "Soulbound", "Spiritbound", "Nightfall", "Dawnbringer", "Twilight", "Starforged", "Sunfire", "Moonshadow", "Voidwalker", "Stormcaller", "Firewalker", "Icebreaker", "Earthwarden"]
-    suffixes = ["of Power", "of Agility", "of Strength", "of Wisdom", "of Resilience", "of Fortitude", "of Endurance", "of Protection", "of the Guardian", "of the Sentinel", "of the Protector", "of the Defender", "of the Champion", "of the Conqueror", "of the Vanquisher", "of the Slayer", "of the Hunter", "of the Beastmaster", "of the Warlord", "of the Berserker", "of the Gladiator", "of the Duelist", "of the Assassin", "of the Rogue", "of the Trickster", "of the Shadow", "of the Nightblade", "of the Phantom", "of the Specter"
-                    "of the Wraith", "of the Revenant", "of the Lich", "of the Necromancer", "of the Warlock", "of the Sorcerer", "of the Wizard", "of the Mage", "of the Alchemist", "of the Enchanter", "of the Illusionist", "of the Elementalist", "of the Pyromancer", "of the Cryomancer", "of the Geomancer", "of the Aeromancer", "of the Hydromancer", "of the Chronomancer", "of the Necromancer", "of the Summoner"]
+    #suffixes = ["of Power", "of Agility", "of Strength", "of Wisdom", "of Resilience", "of Fortitude", "of Endurance", "of Protection", "of the Guardian", "of the Sentinel", "of the Protector", "of the Defender", "of the Champion", "of the Conqueror", "of the Vanquisher", "of the Slayer", "of the Hunter", "of the Beastmaster", "of the Warlord", "of the Berserker", "of the Gladiator", "of the Duelist", "of the Assassin", "of the Rogue", "of the Trickster", "of the Shadow", "of the Nightblade", "of the Phantom", "of the Specter"
+               #     "of the Wraith", "of the Revenant", "of the Lich", "of the Necromancer", "of the Warlock", "of the Sorcerer", "of the Wizard", "of the Mage", "of the Alchemist", "of the Enchanter", "of the Illusionist", "of the Elementalist", "of the Pyromancer", "of the Cryomancer", "of the Geomancer", "of the Aeromancer", "of the Hydromancer", "of the Chronomancer", "of the Necromancer", "of the Summoner"]
 
-    if type = "Heavy":
-        Material = ["Steel", "Iron", "Bronze", "Titanium", "Adamantium", "Obsidian", "Dragonhide"]
-    elif type = "Medium":
-        Material = ["Composite", "Plate","Composite", "Plate", "Scale Mail", "Brigandine", "Lamellar", "Cuirass", "Hauberk", "Studded Leather"]
-    elif type = "Light":
+    setbonusn = ["Orion the Hunter", "The Beastmaster", "The Berserker", "The Gladiator", "The Duelist", "The Assassin",
+                "The Rogue", "The Trickster", "The Shadow", "The Nightblade", "The Phantom", "The Specter", "The Wraith",
+                "The Revenant", "The Lich", "The Necromancer", "The Warlock", "The Sorcerer", "The Wizard", "The Mage",
+                "The Alchemist", "The Enchanter", "The Illusionist", "The Elementalist", "The Pyromancer", "The Cryomancer",
+                "The Geomancer", "The Aeromancer", "The Hydromancer"]
+
+    if specproc == None:
+        specprocn = 0
+    elif sum(1 for proc in specproc) == 2:
+        specprocn = str(random.choice(specproc))
+    elif sum(1 for proc in specproc) == 3:
+        specprocn = str(random.choice(specproc))
+    else:
+        specprocn = str(specproc)  # Choose a random special proc from the list
+
+    if type == "Heavy":
+        Material = ["Steel", "Iron", "Bronze", "Titanium", "Adamantium", "Obsidian"]
+    elif type == "Medium":
+        Material = ["Composite", "Plate", "Composite", "Plate", "Scale Mail", "Brigandine", "Dragonhide", "Lamellar", "Cuirass", "Hauberk", "Studded Leather"]
+    elif type == "Light":
         Material = ["Leather", "Bone", "Hide", "Fur", "Scale", "Chainmail"]
-    elif type = "Cloth":
+    elif type == "Cloth":
         Material = ["Cotton", "Linen", "Wool", "Silk", "Velvet", "Satin", "Brocade"]
 
-    if slot = "Head":
+    if slot == "Head":
         slotname = random.choice(["Helm", "Crown", "Cap", "Mask"])
-    elif slot = "Shoulder":
-        slotname = random.choice(["Pauldrons", "Spaulders", "Shoulder Guards", "Shoulder Pads", "Shoulder Plates", "Shoulder Armor", "Shoulder Capes", "Shoulder Cloaks"])
-    elif slot = "Arm":
+    elif slot == "Shoulder":
+        slotname = random.choice(["Pauldrons", " Spaulders", "Shoulder Guards", "Shoulder Pads", "Shoulder Plates", "Shoulder Armor", "Shoulder Capes", "Shoulder Cloaks"])
+    elif slot == "Arm":
         slotname = random.choice(["Bracers", "Arm Guards", "Bicep Guards", "Sleeves", "Vambraces", "Cuffs", "Armlets"])
-    elif slot = "Hand":
+    elif slot == "Hand":
         slotname = random.choice(["Gloves", "Gauntlets", "Mittens"])
-    elif slot = "Chest":
+    elif slot == "Chest":
         slotname = random.choice(["Chestplate", "Cuirass", "Hauberk"])
-    elif slot = "Pants":
+    elif slot == "Pants":
         slotname = random.choice(["Leggings", "Pants", "Trousers"])
-    elif slot = "Boots":
+    elif slot == "Boots":
         slotname = random.choice(["Boots", "Shoes", "Sandals"])
 
-    # Choose a random prefix and suffix
-    prefix = random.choice(prefixes)
-    suffix = random.choice(suffixes)
+    if rating > 90:
+        ratingname = "Ascended"
+    elif rating > 85:
+        ratingname = "Celestial"
+    elif rating > 80:
+        ratingname = "Exalted"
+    elif rating > 75:
+        ratingname = "Mythic"
+    elif rating > 70:
+        ratingname = "Legendary"
+    elif rating > 65:
+        ratingname = "Epic"
+    elif rating > 60:
+        ratingname = "Superior"
+    elif rating > 55:
+        ratingname = "Ornate"
+    elif rating > 50:
+        ratingname = "Distinguished"
+    elif rating > 45:
+        ratingname = "Sturdy"
+    elif rating > 40:
+        ratingname = "Notable"
+    elif rating > 35:
+        ratingname = "Uncommon"
+    else:
+        ratingname = "Common"
 
+    # Choose a random prefix and suffix
+    #prefix = random.choice(prefixes)
+    #suffix = random.choice(suffixes)
+
+    protfillerlist = ("protection", "resistance", "defense", "toughness", "fortification",
+                      "shielding", "guardianship", "safeguarding", "deflection", "absorption")
+    protectfiller = random.choice(protfillerlist)
     # Combine them with the type and slot to create the item name
-    return f"{prefix} {type} {slot} {suffix}"
+    materialname = random.choice(Material)
+
+    if setbonus == None:
+        if specprocn == 0:
+            returnname = (f"{ratingname} {materialname} {slotname}") #no set bonus or special proc
+        else:
+            returnname = (f"{ratingname} {materialname} {slotname} of {specprocn} {protectfiller}") #special proc but no set bonus
+    else:
+        if specprocn == 0:
+            returnname = (f"{ratingname} {materialname} {slotname} of {setbonusn}") #set bonus but no special proc
+        else:
+            returnname = (f"{ratingname} {materialname} {slotname} of {setbonusn} and {specprocn} {protectfiller}") #set bonus and special proc
+
+    return returnname
